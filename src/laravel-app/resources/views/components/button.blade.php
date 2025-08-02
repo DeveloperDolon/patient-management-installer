6645cbdf5e696db93b3c6a7642636b4bf9a03c65
DeveloperDolon <dolonr718@gmail.com>
2025-08-02 22:28:49 +0600

    file removed

--- src/laravel-app/resources/views/components/button.blade.php
+++
@@ -1,5 +0,0 @@
-<button
-    class="inline-flex h-full items-center justify-center rounded bg-indigo-700 px-6 py-3 hover:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none"
->
-    <p class="text-sm leading-none font-medium text-nowrap text-white">{{ $data['content'] }}</p>
-</button>
